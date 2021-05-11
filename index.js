7726e2dce55a2bee2321ab70ea1832a7b1e0d6db
mich360 <65708357+mich360@users.noreply.github.com>
2021-05-11 15:53:36 +0900

    test2
    nane export の　alias　（あだ名）

---
+++ index.js
@@ -0,0 +1,5 @@
+import { mx ,mz ,long } from "./user.js";
+
+long(mx);
+
+document.body.textContent = mx + mz;
---
+++ user.js
@@ -0,0 +1,8 @@
+const name = "みっちゃん";
+const name2 = "まっちゃんちゃん";
+
+function log(value) {
+    console.log(value);
+}
+
+export { name as  mx,name2 as mz,log as long};
